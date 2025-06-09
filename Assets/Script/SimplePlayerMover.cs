a114f5bb6c6733921ba3625fb8a3468cf525d713
hinataze <hermenegildosolheiro@icloud.com>
2025-06-09 16:51:47 +0900

    first

---
+++ Assets/Script/SimplePlayerMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class SimplePlayerMover : MonoBehaviourPun
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        if (!photonView.IsMine) return; // Only move your own cube
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v) * moveSpeed * Time.deltaTime;
+        transform.Translate(move, Space.World);
+    }
+}
---
+++ Assets/Script/VoiceNetworkManager.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class VoiceNetworkManager : MonoBehaviourPunCallbacks
+{
+    public GameObject playerPrefab;
+
+    void Start()
+    {
+
+        Debug.Log("✅ VoiceStartupCheck: Script is running!");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("VoiceRoom", new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Vector3 pos = new Vector3(Random.Range(-2, 2), 1, Random.Range(-2, 2));
+        PhotonNetwork.Instantiate("PLAYER", pos, Quaternion.identity);
+    }
+
+
+}
