c7e7a5773ace5a3c2935687511286f50c7c86e62
hinataze <hermenegildosolheiro@icloud.com>
2025-06-21 12:14:07 +0100

    new XR framework to  build each plug in test to  HMD

---
+++ Assets/Script/XRTrackingDisabler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using System.Collections.Generic;
+
+/// <summary>
+/// Disables XR headset tracking so that transform.position and rotation can be controlled by script.
+/// </summary>
+public class XRTrackingDisabler : MonoBehaviour
+{
+    void Start()
+    {
+        // Disable XR positional tracking (keep device mode, not floor)
+        List<XRInputSubsystem> xrSystems = new List<XRInputSubsystem>();
+        SubsystemManager.GetInstances(xrSystems);
+
+        foreach (var xr in xrSystems)
+        {
+            if (xr.TrySetTrackingOriginMode(TrackingOriginModeFlags.Device))
+            {
+                Debug.Log("[XRTrackingDisabler] Set tracking origin to Device.");
+            }
+
+            if (xr.TryRecenter())
+            {
+                Debug.Log("[XRTrackingDisabler] XR headset recentered.");
+            }
+        }
+    }
+}
